fee9050645d5b23b5991d55c8e4b8d24b776aa3b
Rutik Patil <rutik4663@gmail.com>
2024-02-15 00:13:04 +0530

    Initial Commit

---
+++ portfolio/views.py
@@ -0,0 +1,9 @@
+
+
+# Create your views here.
+from django.shortcuts import render
+from .models import Project
+
+def portfolio(request):
+    projects = Project.objects.all()
+    return render(request, 'portfolio/index.html', {'projects': projects})
